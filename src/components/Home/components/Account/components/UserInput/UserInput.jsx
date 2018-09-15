c66a088485a4f658a4a38e6060e71c6281d38cbd
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-15 17:20:12 -0400

    Add User component to show input

---
+++ src/components/Home/components/Account/components/UserInput/UserInput.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    const {handleGoogleLogin} = this.props
+    return (
+      <div className="footer">
+        PLEASE ENTER YOUR PHONE NUMBER
+      </div>
+    )
+  }
+}
---
+++ src/components/Home/components/Account/components/UserInput/index.jsx
@@ -0,0 +1,17 @@
+import UserInput from './UserInput';
+
+export default UserInput;
+
+
+
+// if(sender.toFrom == null) {
+//   send directions
+//   update user toFrom = GeoFrom, getPortPromise
+// } else {
+//   if(message === UBER) {
+//     tell then how long it will take and price
+//   } else if( message === CONFIRM) {
+//     tell them the number and name of the uber 
+//     reset toFrom === null
+//   } 
+// }
