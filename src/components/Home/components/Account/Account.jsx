e4bd9d3c638751b71428d8b83868fdf0f6d8221e
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-15 15:11:24 -0400

    Add Acount skeleton

---
+++ src/components/Home/components/Account/Account.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Account extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="welcome2">
+          <div className="logoText">
+            <p><em>Walkify</em></p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
