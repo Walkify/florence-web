7fe73307a411d776cd1f4a5520c0cd29899c0180
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-15 17:19:08 -0400

    Add account to Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Home.css'
-import { Header, Explain, Footer, Onboard } from './components'
+import { Account, Header, Explain, Footer, Onboard } from './components'
+
 
 export default class Home extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Home extends Component {
         <div className="sep-1" />
         <Explain />
         <Onboard />
+        <Account />
         <Footer />
       </React.Fragment>
     )
