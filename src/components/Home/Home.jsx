f994eb35a6686254ebced136ef6d06daf5184f7c
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-16 06:26:56 -0400

    Fix so not refetch token

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,19 +7,8 @@ import { fire } from "../../config/constants";
 class Home extends Component {
   constructor() {
     super()
-    const auth_code = window.location.href.split('/').slice(-1).pop() ? 
-      window.location.href.split('/').slice(-1).pop().substr(6) : null
-    
-    if(auth_code) {
-      fire.database().ref('users/' + localStorage.getItem('appToken') + '/uber').set({
-        access_token: null,
-        auth_code,
-        redirect_uri: window.location.href.split('?code')[0]
-      })
-    }
     this.state = {
       auth: false,
-      auth_code,
       uberToken: null,
       access_token: null,
     }
@@ -32,9 +21,18 @@ class Home extends Component {
     if (localStorage.getItem(firebaseAuthKey) === "1") {
       let userId = localStorage.getItem('appToken');
       let uberToken = null; 
-      fire.database().ref('/users/'+userId).once('value').then((uid) => this.setState({
-        access_token: uid.val().uber.access_token.access_token || false}));
-      
+      await fire.database().ref('/users/'+userId).once('value').then((uid) => this.setState({
+        access_token: uid.val().uber.access_token ? uid.val().uber.access_token.access_token : false, auth_code: uid.val().uber.auth_code }));
+
+      if(!this.state.access_token && this.state.auth_code) {
+        const auth_code = window.location.href.split('/').slice(-1).pop() ? 
+        window.location.href.split('/').slice(-1).pop().substr(6) : null
+        fire.database().ref('users/' + localStorage.getItem('appToken') + '/uber').set({
+          access_token: null,
+          auth_code,
+          redirect_uri: window.location.href.split('?code')[0]
+        })
+      }
     }
   }
   render() {
