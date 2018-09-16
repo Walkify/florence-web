7a780dd17689c533ba1803876a3cf6f21a4a00c1
Calum Patrick <hello@calum.co>
2018-09-16 06:03:38 -0400

    it works

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,14 +5,9 @@ import { fire } from "../../config/constants";
 
 
 class Home extends Component {
-  componentDidMount() {
-    const firebaseAuthKey = "firebaseAuthInProgress";
-    if (localStorage.getItem(firebaseAuthKey) === "1" ) {
-      this.setState({auth: true})
-    }
-  }
   constructor() {
     super()
+    
     const uberCode = window.location.href.split('/').slice(-1).pop() ? 
       window.location.href.split('/').slice(-1).pop().substr(6) : null
     
@@ -23,18 +18,33 @@ class Home extends Component {
     }
     this.state = {
       auth: false,
+      uberToken: null,
+      access_token: null,
       uberCode
     }
   }
+  async componentDidMount() {
+    const firebaseAuthKey = "firebaseAuthInProgress";
+    if (localStorage.getItem(firebaseAuthKey) === "1" ) {
+      this.setState({auth: true})
+    }
+    if (localStorage.getItem(firebaseAuthKey) === "1") {
+      let userId = localStorage.getItem('appToken');
+      let uberToken = null; 
+      fire.database().ref('/users/'+userId).once('value').then((uid) => this.setState({
+        uberToken: uid.node_.children_.root_.value.value_ || false})); //access_token
+      
+    }
+  }
   render() {
-    const { uberCode } = this.state 
+    const { uberToken } = this.state 
     return (
       <React.Fragment>
         <Header />
         <div className="sep-1" />
         <Explain />
         {!this.state.auth && <Onboard />}
-        <Account uberDisabled={!uberCode} />
+        <Account uberDisabled={!uberToken} />
         <Footer />
       </React.Fragment>
     )
