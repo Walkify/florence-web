3e78917cbf9b3d032a252bede27989630ba56c97
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-16 10:27:33 -0400

    overhaul of home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,50 +5,45 @@ import { fire } from "../../config/constants";
 
 
 class Home extends Component {
-  constructor() {
-    super()
-    this.state = {
-      auth: false,
-      uberToken: null,
-      access_token: null,
-    }
+  state = {
+    auth: null,
+    access_token: null,
+    auth_code: null
+
+  }
+  accountInfo = async () => {
+    let account = await fire.database().ref('/users/'+ localStorage.getItem('appToken')).once('value').then((user) => user.val())
+    let auth = localStorage.getItem('appToken')
+    let access_token = account ? account.uber ? account.uber.access_token ? account.uber.access_token.access_token : false : false : false
+    let auth_code = account ? account.uber ? account.uber.auth_code : false : false
+    auth_code = window.location.href.split('/').slice(-1).pop() 
+        ? window.location.href.split('/').slice(-1).pop().substr(6) : auth_code
+
+    console.log(auth, access_token, auth_code)
+    this.setState({ auth, access_token, auth_code })
   }
   async componentDidMount() {
-    const firebaseAuthKey = "firebaseAuthInProgress";
-    if (localStorage.getItem(firebaseAuthKey) === "1" ) {
-      this.setState({auth: true})
-    }
-    if (localStorage.getItem(firebaseAuthKey) === "1") {
-      let userId = localStorage.getItem('appToken');
-      await fire.database().ref('/users/'+userId).once('value').then((uid) => {
-        if(uid.val()){
-        const data = uid.val().uber
-        const access_token = data && data.access_token ? data.access_token.access_token : null
-        const auth_code = data && data.auth_code ? data.auth_code : null
-        this.setState({access_token, auth_code});
-        }
+    await this.accountInfo()
+    let { auth, access_token, auth_code } = this.state
+
+    if(auth && auth_code) {
+      console.log
+      fire.database().ref('users/' + localStorage.getItem('appToken') + '/uber').set({
+        access_token: access_token || false,
+        auth_code: auth_code ||  false,
+        auth: auth || false,
+        redirect_uri: window.location.href.split('?code')[0]
       })
-      const auth_code = window.location.href.split('/').slice(-1).pop() ? 
-      window.location.href.split('/').slice(-1).pop().substr(6) : null
-      if(!this.state.access_token || auth_code) {
-        fire.database().ref('users/' + localStorage.getItem('appToken') + '/uber').set({
-          access_token: null,
-          auth_code,
-          redirect_uri: window.location.href.split('?code')[0]
-        })
-      }
     }
   }
   render() {
-    const { access_token } = this.state 
-
     return (
       <React.Fragment>
         <Header />
         <div className="sep-1" />
         <Explain />
         {!this.state.auth && <Onboard />}
-        <Account uberDisabled={!access_token} />
+        <Account uberDisabled={this.state.access_token} />
         <Footer />
       </React.Fragment>
     )
