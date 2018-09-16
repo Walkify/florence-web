9e50279a6c624cfd75e9185e2d8ad32febd5e06b
Calum Patrick <hello@calum.co>
2018-09-16 03:25:33 -0400

    button auth states

--- src/components/Home/components/Account/components/UserInput/UserInput.jsx
+++ src/components/Home/components/Account/components/UserInput/UserInput.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './UserInput.css'
 
+import { fire } from "../../../../../../config/constants";
+
 const firebaseAuthKey = "firebaseAuthInProgress";
 
 export default class UserInput extends Component {
@@ -8,16 +10,17 @@ export default class UserInput extends Component {
     super(props);
     this.state = {
       number: null,
-      uberAuth: false,
     }
     this.submitNumber = this.submitNumber.bind(this);
-    this.submitUberw
     this.handleChange = this.handleChange.bind(this);
   }
 
   submitNumber(event) {
     console.log(this.state.number)
     console.log(localStorage.getItem(firebaseAuthKey));
+    fire.database().ref('users/' + localStorage.getItem('appToken')).update({
+      "phoneNumber": this.state.number,
+    })
     event.preventDefault();
   }
 
@@ -28,7 +31,7 @@ export default class UserInput extends Component {
   }
 
   render() {
-    const { handleGoogleLogin } = this.props
+    const { handleGoogleLogin, uberDisabled } = this.props
     const buttonMessage = this.props.uberDisabled ? "Sign in to Uber" : "Authorized!"
     return (
       <div className="">
@@ -49,7 +52,7 @@ export default class UserInput extends Component {
             <div className="rightAuth">
               <label>Uber Sign In:</label><br />
               <a href={`https://login.uber.com/oauth/v2/authorize?client_id=aV2-ngv_423bZyLlNBfZ6iUEVoDNESdN&response_type=code&scope=profile&grant_type=authorization_code&redirect_uri=${window.location.href}`}>
-                <button href="#" className={this.state.uberAuth ? "completedButton" : "submitButton"}>{this.state.uberAuth ? "Signed in already" : "Sign in to Uber"}</button>
+                <button className={!uberDisabled ? "completedButton" : "submitButton"}>{!uberDisabled ? "Signed in already" : "Sign in to Uber"}</button>
               </a>
             </div>
           </div>
